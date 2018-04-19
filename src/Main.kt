c010314b5d6c02f09e7ddc45636ee111718bb54c
Hardeep <hardeep.singh@mobileforming.com>
2018-04-18 19:53:26 -0700

    Done

---
+++ src/Main.kt
@@ -0,0 +1,117 @@
+import java.io.File
+
+/**
+ * Define class purpose here: Distance Calculations
+ *
+ * Created by hardeepsingh
+ * Date: April 18, 2018.
+ */
+data class GeoPoint(var latitude: Double, var longitude: Double)
+
+data class GeoPointInfo(var pathGeoPoint: GeoPoint, var locationGeoPoint: GeoPoint,
+                        var distance: Double, var residual: GeoPoint?) {
+    override fun toString(): String {
+        return "Path Point: $pathGeoPoint | Location Point : $locationGeoPoint " +
+                "| Distance = ${"%.5f".format(distance)} | Residual: $residual"
+    }
+}
+
+data class GeoCalculationResults(var geoPointInfoList: List<GeoPointInfo>, var rmseLat: Double,
+                                 var rmseLng: Double, var rmseDist: Double) {
+}
+
+fun main(args: Array<String>) {
+    val pathList = mutableListOf<GeoPoint>()
+    val locationList = mutableListOf<GeoPoint>()
+
+    //Read path way-points file
+    File("path_gm_17_false.txt").useLines { lines ->
+        lines.forEach {
+            val result = it.split(",")
+            pathList.add(GeoPoint(result[0].toDouble(), result[1].toDouble()))
+        }
+    }
+
+    //Read user movement file
+    File("user_location_17_false.txt").useLines { lines ->
+        lines.forEach {
+            val result = it.split(",")
+            locationList.add(GeoPoint(result[0].toDouble(), result[1].toDouble()))
+        }
+    }
+    println("Path List Size: ${pathList.size}")
+    println("Location List Size: ${locationList.size}")
+
+    //Find closest Location GEO points from Path file and store it
+    val geoPointInfo = mutableListOf<GeoPointInfo>()
+    locationList.forEach({
+        val location = it
+        var shortestDistance = Double.MAX_VALUE
+        var shortDistancePoint: GeoPoint? = null
+        pathList.forEach({
+            val distance = getDistanceInMeters(location, it)
+            if (shortestDistance > distance) {
+                shortestDistance = distance
+                shortDistancePoint = it
+            }
+        })
+        geoPointInfo.add(GeoPointInfo(shortDistancePoint!!, location, "%.5f".format(shortestDistance).toDouble(), null))
+    })
+
+    //Get only Distinct
+    val distinctGeoPointInfo = geoPointInfo.distinctBy { it.pathGeoPoint }
+    println("GeoPointInfo Size: ${distinctGeoPointInfo.size}")
+
+    //Calculate Residual
+    distinctGeoPointInfo.forEach({
+        it.residual = GeoPoint(it.pathGeoPoint.latitude - it.locationGeoPoint.latitude,
+                    it.pathGeoPoint.longitude - it.locationGeoPoint.longitude)
+        println(it)
+    })
+
+    val (rmseLat, rmseLng, rmseDist) = rootMeanSquareError(distinctGeoPointInfo)
+    println("Root Mean Square Error Latitude: $rmseLat")
+    println("Root Mean Square Error Longitude: $rmseLng")
+    println("Root Mean Square Error Distance: $rmseDist")
+
+    //Create Result Object and Write to Result File
+    var geoCalculationResults = GeoCalculationResults(distinctGeoPointInfo, rmseLat, rmseLng, rmseDist)
+    File("result_calculation_17_false.txt").printWriter().use { out ->
+        geoCalculationResults.geoPointInfoList.forEach({
+            out.write(it.toString() + "\n")
+        })
+        out.write("Root Mean Square Error Latitude: ${geoCalculationResults.rmseLat} \n")
+        out.write("Root Mean Square Error Longitude: ${geoCalculationResults.rmseLng} \n")
+        out.write("Root Mean Square Error Distance: ${geoCalculationResults.rmseDist}")
+    }
+    println("Process Finished!!")
+}
+
+fun rootMeanSquareError(data: List<GeoPointInfo>): List<Double> {
+    var totalLat = 0.0
+    var totalLng = 0.0
+    var totalDistance = 0.0
+    var n = data.size
+    data.forEach({
+        totalLat += Math.pow(it.residual!!.latitude, 2.0)
+        totalLng += Math.pow(it.residual!!.latitude, 2.0)
+        totalDistance += Math.pow(it.distance, 2.0)
+    })
+
+    return listOf(Math.sqrt(totalLat/n), Math.sqrt(totalLng/n), Math.sqrt(totalDistance/n))
+}
+
+fun getDistanceInMeters(point1: GeoPoint, point2: GeoPoint): Double {
+    val r = 6371
+    val lat1 = point1.latitude
+    val lng1 = point1.longitude
+    val lat2 = point2.latitude
+    val lng2 = point2.longitude
+    val dLat = Math.toRadians(lat2 - lat1)
+    val dLon = Math.toRadians(lng2 - lng1)
+    val a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + (Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+            * Math.sin(dLon / 2) * Math.sin(dLon / 2))
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    return r * c * 1000
+}
+
